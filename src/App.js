34583f89d3228734eaaaadb48b4d29e1166473f0
mia-chou <mia.fy.chou@hotmail.com>
2024-03-16 13:18:11 +0000

    Added grid to cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
+import React, { useState } from 'react';
 import './App.css';
 
-const cardImages=[
-{"src":"/images-helemet-1.png"},
-{"src":"/images-potion-1.png"},
-{"src":"/images-ring-1.png"},
-{"src":"/images-scroll-1.png"},
-{"src":"/images-shield-1.png"},
-{"src":"/images-sword-1.png"},
-
-
-]
-
-
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+];
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  // Shuffle cards
+  const shuffleCards = () => {
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card, index) => ({ ...card, id: index + 1 }));
+    setCards(shuffleCards);
+    setTurns(0);
+  };
+
   return (
     <div className="App">
-
-       <h1>Magic Match</h1>
-      <button>New Game</button>
-      
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+      <div className="card-grid">
+        {cards.map(card => (
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card-front" />
+              <img className="back" src="/img/cover.png" alt="card-back" />
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
