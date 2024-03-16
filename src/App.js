23411cfc2859d8f119d98af6faae2db3da4904b3
mia-chou <mia.fy.chou@hotmail.com>
2024-03-16 06:51:26 +0000

    Added images

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+
+const cardImages=[
+{"src":"/images-helemet-1.png"},
+{"src":"/images-potion-1.png"},
+{"src":"/images-ring-1.png"},
+{"src":"/images-scroll-1.png"},
+{"src":"/images-shield-1.png"},
+{"src":"/images-sword-1.png"},
+
+
+]
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+       <h1>Magic Match</h1>
+      <button>New Game</button>
+      
+    </div>
+  );
+}
+
+export default App;
